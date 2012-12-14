5cf7c511ead052465cae05571c3d915feed6f4bb
Jacob Dalton <jacobrdalton@gmail.com>
2012-12-14 14:30:14 -0600

    Blarghy fixy mcfixins. Fixed links in facebook.js

--- facebook.js
+++ facebook.js
@@ -86,7 +86,7 @@ function postToMyFeed()
 {
 	executeOnAuth(function(response) {
 		var messageText = $j('#ose-truefan-friends-message').val();
-		var userLink = $j('#user-video-link').html();
+		var userLink = $j('#user-video-link').val();
 		var postData =
 		{
 			message: messageText,
